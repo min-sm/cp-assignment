2bac62f946b27aaa99a045bfb70ff98ddafcfe4b
mtm.minsoemoe <mtm.minsoemoe@gmail.com>
2024-12-19 08:42:24 +0000

    footer home route change

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,13 +2,13 @@
 <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <a href="{{ route('home') }}" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <img src="{{ asset('img/logo.png') }}" class="h-8" alt="Tech Giant Logo" />
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tech Giant</span>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="{{ route('/') }}" class="hover:underline me-4 md:me-6">Home</a>
+                    <a href="{{ route('home') }}" class="hover:underline me-4 md:me-6">Home</a>
                 </li>
                 <li>
                     <a href="#" class="hover:underline me-4 md:me-6">About</a>
@@ -25,7 +25,7 @@
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="{{ route('/') }}" class="hover:underline">Tech Giant™</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="{{ route('home') }}" class="hover:underline">Tech Giant™</a>. All Rights Reserved.</span>
     </div>
 </footer>
 
