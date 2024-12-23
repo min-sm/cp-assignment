387af8be8873623f0c556b54dfa12d3dfb730fd9
mtm-minsoemoe <minsoemoe@metateammyanmar.com>
2024-12-23 17:06:43 +0000

    category migration & seeder fixed value

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            ['name' => 'Laptops', 'description' => 'All types of laptops.'],
+            ['name' => 'Adapters', 'description' => 'Laptop power adapters.'],
+            ['name' => 'Keyboards', 'description' => 'External keyboards for laptops.'],
+            ['name' => 'Batteries', 'description' => 'Laptop batteries.'],
+            ['name' => 'Screens', 'description' => 'Laptop screens and displays.'],
+            ['name' => 'Mice', 'description' => 'External mice for laptops.'],
+        ];
+
+        DB::table('categories')->insert($categories);
     }
 }
