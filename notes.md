731e508a61a2fc8a266dfa9af0ff202299e1f279
Min Soe Moe <minsoemoe999@gmail.com>
2025-01-07 00:03:47 +0630

    debugbar, socialite added
    not finished yet

---
+++ notes.md
@@ -0,0 +1,5 @@
+Color palette link - https://colorhunt.co/palette/1a363640534c677d6ad6bd98
+#1A3636
+#40534C
+#677D6A
+#D6BD98
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,6 +17,40 @@
 
             <x-form.password name="password" label="Your password" />
 
+            <div class="flex items-center justify-center my-4">
+                <div class="h-px bg-gray-300 flex-grow"></div>
+                <span class="px-4 text-gray-500">or</span>
+                <div class="h-px bg-gray-300 flex-grow"></div>
+            </div>
+
+            <div class="text-center mb-5">
+                <p class="text-gray-600 mb-4">Login using social networks</p>
+                <div class="flex justify-center gap-4">
+                    <!-- Facebook Button -->
+                    <a href="#" class="bg-[#1877F2] rounded-full w-10 h-10 flex items-center justify-center">
+                        <span class="text-lg">
+                            <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                    d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                    </a>
+                    <!-- Google Button -->
+                    <a href="#" class="bg-red-500 text-white rounded-full w-10 h-10 flex items-center justify-center">
+                        <span class="text-lg">
+                            <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                    d="M12.037 21.998a10.313 10.313 0 0 1-7.168-3.049 9.888 9.888 0 0 1-2.868-7.118 9.947 9.947 0 0 1 3.064-6.949A10.37 10.37 0 0 1 12.212 2h.176a9.935 9.935 0 0 1 6.614 2.564L16.457 6.88a6.187 6.187 0 0 0-4.131-1.566 6.9 6.9 0 0 0-4.794 1.913 6.618 6.618 0 0 0-2.045 4.657 6.608 6.608 0 0 0 1.882 4.723 6.891 6.891 0 0 0 4.725 2.07h.143c1.41.072 2.8-.354 3.917-1.2a5.77 5.77 0 0 0 2.172-3.41l.043-.117H12.22v-3.41h9.678c.075.617.109 1.238.1 1.859-.099 5.741-4.017 9.6-9.746 9.6l-.215-.002Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                    </a>
+                </div>
+            </div>
+
             <div class="flex items-start mb-5">
                 <div class="flex items-center h-5">
                     <input id="remember" type="checkbox" value="" name="remember"
