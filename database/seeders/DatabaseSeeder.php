ff58aba5e02e51b166c2fe53740c61be0ad926a1
mtm-minsoemoe <minsoemoe@metateammyanmar.com>
2025-01-11 04:04:01 +0000

    changes in db schema
    - php artisan db:wipe
    - php artisan migrate:refresh --seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Serie;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +17,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Brand::factory()->count(10)->create();
+
+        $categories = [
+            [
+                'name' => 'Laptops',
+                'description' => 'All types of laptops.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Adapters',
+                'description' => 'Laptop power adapters.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Keyboards',
+                'description' => 'External keyboards for laptops.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Batteries',
+                'description' => 'Laptop batteries.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Screens',
+                'description' => 'Laptop screens and displays.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Mice',
+                'description' => 'External mice for laptops.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        DB::table('categories')->insert($categories);
+        Brand::factory()->count(5)->create();
         Serie::factory()->count(10)->create();
         Product::factory()->count(50)->create();
     }
