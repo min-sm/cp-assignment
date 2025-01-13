dd1b81998d4d04566950daaacc394bf490a8c8e0
mtm-minsoemoe <minsoemoe@metateammyanmar.com>
2025-01-13 13:28:44 +0000

    trying to use get method in product card

--- resources/views/components/products/grid.blade.php
+++ resources/views/components/products/grid.blade.php
@@ -3,7 +3,8 @@
 <div class="max-w-screen-xl mx-auto p-4">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         @foreach ($products as $product)
-            <x-products.card :product="$product" />
+            {{-- <x-products.card :product="$product" /> --}}
+            @livewire('products-index', ['product' => $product])
         @endforeach
     </div>
 
