1a2ee14dac83265f5b722bcc441fab9d32edb8d8
mtm-minsoemoe <minsoemoe@metateammyanmar.com>
2025-01-15 08:02:59 +0000

    cart created

--- resources/views/components/products/card.blade.php
+++ resources/views/components/products/card.blade.php
@@ -39,10 +39,10 @@
             {{ Str::words($product->description, 15, '...') }}
         </p>
         <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">${{ $product->price }}</span>
-            <a href=""
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Add to cart
+            <span class="text-xl xl:text-3xl font-bold text-gray-900 dark:text-white">${{ $product->price }}</span>
+            <a href="{{ route('product.show', ['slug' => $product->slug]) }}"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-1.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Details
             </a>
         </div>
     </div>
