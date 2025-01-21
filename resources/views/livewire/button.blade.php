719bdee2a956a55bac1a4a70b237d94b35522e52
mtm-minsoemoe <minsoemoe@metateammyanmar.com>
2025-01-21 16:30:46 +0000

    working on add to cart to cart in header

---
+++ resources/views/livewire/button.blade.php
@@ -0,0 +1,5 @@
+<button
+    class="bg-indigo-600 flex gap-2 items-center text-white px-6 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+    type="button" wire:click="clicked">
+    Add to Cart
+</button>
