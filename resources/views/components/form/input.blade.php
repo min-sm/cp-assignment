c4d5648e1f219b526e0bca77c8c9e9a2299aca1f
min-sm <minsoemoe999@gmail.com>
2025-02-19 13:18:49 +0000

    [shop] added inputAttributes props in form.input

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,4 @@
-@props(['name', 'label', 'type' => 'text', 'placeholder' => '', 'required' => false])
+@props(['name', 'label', 'type' => 'text', 'placeholder' => '', 'required' => false, 'inputAttributes' => []])
 
 <div {{ $attributes }}>
     <label for="{{ $name }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -6,18 +6,16 @@
     </label>
     <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ old($name) }}"
         placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}
-        {{ $attributes->merge([
-            'class' =>
-                'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                            focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-                            dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-                            dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ' .
-                ($errors->has($name)
-                    ? 'bg-red-50 border-red-500 text-red-900
-                            placeholder-red-700 dark:text-red-500 dark:placeholder-red-500
-                            dark:border-red-500'
-                    : ''),
-        ]) }} />
+        @foreach ($inputAttributes as $attribute => $value)
+            {{ $attribute }}="{{ $value }}" @endforeach
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+               focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+               dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+               dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+               {{ $errors->has($name)
+                   ? 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'
+                   : '' }}" />
+
     @error($name)
         <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
     @enderror
