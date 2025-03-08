43cf2c7229446dea08f5319cdbf337ed5b33ace2
min-sm <minsoemoe999@gmail.com>
2025-03-08 08:21:23 +0000

    input component changes

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -78,27 +78,10 @@
                         type="number" :inputAttributes="['min' => 1]" />
                     <x-form.input name="price" label="Price" placeholder="Enter price" :required="true"
                         type="number" :inputAttributes="['min' => 1]" />
-
-                    <div class="sm:col-span-2">
-                        <label for="description"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                        <textarea id="description" rows="8" name="description"
-                            class="block p-2.5 w-full text-sm rounded-lg border focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500 {{ $errors->has('description')
-                                ? 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'
-                                : 'bg-gray-50 border-gray-300 text-gray-900 dark:text-white dark:placeholder-gray-400 dark:border-gray-600' }}"
-                            placeholder="Product description here">{{ old('description') }}</textarea>
-                        @error('description')
-                            <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
-                        @enderror
-                    </div>
-
-                    <div class="sm:col-span-2">
-                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            for="files">Upload multiple files</label>
-                        <input
-                            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                            id="files" type="file" name="files[]" multiple>
-                    </div>
+                    <x-form.input name="description" label="Description" placeholder="Product description here"
+                        :required="false" element="textarea" class="sm:col-span-2" />
+                    <x-form.input name="files[]" label="Upload multiple files" type="file" :inputAttributes="['multiple' => true]"
+                        class="sm:col-span-2" />
 
                 </div>
                 <button type="submit"
--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,20 +1,54 @@
-@props(['name', 'label', 'type' => 'text', 'placeholder' => '', 'required' => false, 'inputAttributes' => []])
+@props([
+    'name',
+    'label',
+    'type' => 'text',
+    'placeholder' => '',
+    'required' => false,
+    'inputAttributes' => [],
+    'element' => 'input',
+    'inputClass' => '',
+    'rows' => 8,
+])
 
 <div {{ $attributes }}>
-    <label for="{{ $name }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+    <label for="{{ preg_replace('/\[\]$/', '', $name) }}" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
         {{ $label }}
     </label>
-    <input type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ old($name) }}"
-        placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}
-        @foreach ($inputAttributes as $attribute => $value)
-            {{ $attribute }}="{{ $value }}" @endforeach
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-               focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-               dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-               dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
-               {{ $errors->has($name)
-                   ? 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'
-                   : '' }}" />
+
+    @if ($element === 'input')
+        @php
+            if ($type === 'file') {
+                $defaultClass =
+                    'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400';
+            } else {
+                $defaultClass =
+                    'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500';
+            }
+            $errorClass = $errors->has($name)
+                ? 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'
+                : '';
+        @endphp
+        <input type="{{ $type }}" id="{{ preg_replace('/\[\]$/', '', $name) }}" name="{{ $name }}"
+            @if ($type !== 'file') value="{{ old($name) }}"
+                placeholder="{{ $placeholder }}" @endif
+            {{ $required ? 'required' : '' }}
+            @foreach ($inputAttributes as $attribute => $value)
+                {{ $attribute }}="{{ $value }}" @endforeach
+            class="{{ $defaultClass }} {{ $errorClass }} {{ $inputClass }}" />
+    @elseif($element === 'textarea')
+        @php
+            $defaultClass =
+                'block p-2.5 w-full text-sm rounded-lg border focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500 bg-gray-50 border-gray-300 text-gray-900 dark:text-white dark:placeholder-gray-400 dark:border-gray-600';
+            $errorClass = $errors->has($name)
+                ? 'bg-red-50 border-red-500 text-red-900 placeholder-red-700 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500'
+                : '';
+        @endphp
+        <textarea id="{{ $name }}" name="{{ $name }}" rows="{{ $rows }}"
+            placeholder="{{ $placeholder }}" {{ $required ? 'required' : '' }}
+            @foreach ($inputAttributes as $attribute => $value)
+                {{ $attribute }}="{{ $value }}" @endforeach
+            class="{{ $defaultClass }} {{ $errorClass }} {{ $inputClass }}">{{ old($name) }}</textarea>
+    @endif
 
     @error($name)
         <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
